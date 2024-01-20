a714e368ec4fc8420fa36598b247a0687161bc5a
Faheem Shaikh <sf792914@gmail.com>
2024-01-20 17:35:37 +0530

    sql file moved

--- sql/23gca.sql
+++ sql/23gca.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 19, 2024 at 03:08 AM
+-- Generation Time: Jan 11, 2024 at 01:53 PM
 -- Server version: 10.4.28-MariaDB
--- PHP Version: 8.0.28
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,49 @@ SET time_zone = "+00:00";
 --
 -- Database: `23gca`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dislikes`
+--
+
+CREATE TABLE `dislikes` (
+  `dislike_id` int(11) NOT NULL,
+  `post_id` int(11) DEFAULT NULL,
+  `ip_address` varchar(145) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `dislikes`
+--
+
+INSERT INTO `dislikes` (`dislike_id`, `post_id`, `ip_address`, `created_at`) VALUES
+(19, 4, '::1', '2024-01-05 09:31:58'),
+(20, 2, '::1', '2024-01-05 09:32:20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likes`
+--
+
+CREATE TABLE `likes` (
+  `like_id` int(11) NOT NULL,
+  `post_id` int(11) DEFAULT NULL,
+  `ip_address` varchar(145) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `likes`
+--
+
+INSERT INTO `likes` (`like_id`, `post_id`, `ip_address`, `created_at`) VALUES
+(16, 5, '::1', '2024-01-05 09:28:32'),
+(18, 4, '::1', '2024-01-05 09:31:57'),
+(19, 2, '::1', '2024-01-05 09:32:19');
 
 -- --------------------------------------------------------
 
@@ -51,84 +94,30 @@ INSERT INTO `mcq_questions` (`id`, `question_text`, `option1`, `option2`, `optio
 -- --------------------------------------------------------
 
 --
--- Table structure for table `meme_users`
+-- Table structure for table `posts`
 --
 
-CREATE TABLE `meme_users` (
+CREATE TABLE `posts` (
   `id` int(11) NOT NULL,
-  `full_name` varchar(255) NOT NULL,
-  `member_id` varchar(50) DEFAULT NULL,
-  `actuarial_association` varchar(100) DEFAULT NULL,
-  `organisation` varchar(100) DEFAULT NULL,
-  `email` varchar(255) NOT NULL,
-  `contact` varchar(15) DEFAULT NULL,
-  `meme_image_url` varchar(255) DEFAULT NULL,
-  `meme_text` text DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `status` int(11) DEFAULT 1
+  `name` varchar(250) DEFAULT NULL,
+  `email` varchar(250) DEFAULT NULL,
+  `contact` varchar(250) DEFAULT NULL,
+  `member_id` varchar(250) DEFAULT NULL,
+  `caption` text DEFAULT NULL,
+  `image_path` varchar(255) DEFAULT NULL,
+  `status` enum('0','1') NOT NULL DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `meme_users`
+-- Dumping data for table `posts`
 --
 
-INSERT INTO `meme_users` (`id`, `full_name`, `member_id`, `actuarial_association`, `organisation`, `email`, `contact`, `meme_image_url`, `meme_text`, `created_at`, `status`) VALUES
-(1, 'faheem', '2', 'Institute of Actuaries of India', 'iai', 'faheem@actuariesindia.org', '85201236988', 'uploads/memes/logo.jpg', NULL, '2024-01-18 19:49:19', 1),
-(2, 'faheem', '2', 'Institute of Actuaries of India', 'iai', 'faheem@actuariesindia.org', '85201236988', 'uploads/memes/logo.jpg', NULL, '2024-01-18 19:50:51', 1),
-(3, 'faheem', '2323', 'Institute of Actuaries of India', 'iai', 'sk3136537@gmail.com', '85214785698', NULL, 'okokookokkokk', '2024-01-18 20:11:21', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `meme_votes`
---
-
-CREATE TABLE `meme_votes` (
-  `id` int(11) NOT NULL,
-  `user_ip` varchar(250) DEFAULT NULL,
-  `meme_id` int(11) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `status` int(11) DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `speakers`
---
-
-CREATE TABLE `speakers` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `designation` varchar(255) NOT NULL,
-  `bio` text DEFAULT NULL,
-  `linkedin` varchar(255) DEFAULT NULL,
-  `profile_image` varchar(255) DEFAULT NULL,
-  `salutation` varchar(10) DEFAULT NULL,
-  `type` enum('VIP','NON VIP') DEFAULT NULL,
-  `status` int(20) DEFAULT 1,
-  `order_number` int(11) DEFAULT NULL,
-  `url_slug` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `speakers`
---
-
-INSERT INTO `speakers` (`id`, `name`, `designation`, `bio`, `linkedin`, `profile_image`, `salutation`, `type`, `status`, `order_number`, `url_slug`) VALUES
-(2, 'Debasish Panda', 'Chairperson<br>Insurance Regulatory and Development Authority of India', '<p>Mr. Debasish Panda, Chairman at the Insurance Regulatory and Development Authority of India (IRDAI) is an officer of the Indian Administrative Services from the 1987 batch of the Uttar Pradesh cadre. He hails from the state of Odisha.</p>\\r\\n\\r\\n<p>Mr. Panda holds a postgraduate degree in Physics and has also completed a Master&#39;s in Developmental Management. Additionally, he holds an M. Phil degree in Environmental Sciences and has undergone foreign training programs in Public Administration in the United States and the Philippines.</p>\\r\\n\\r\\n<p>He has held various important positions within the Government of India, including Secretary in Department of Financial Services in the Ministry of Finance, Joint Secretary in the Ministry of Health &amp; Family Welfare beside a stint in the Ministry of Commerce. He has also been the District Magistrate and Collector of the districts of Tehri-Garhwal, Uttarkashi, Deoria and Gaziabad in Uttar Pradesh. He was also the Principal Secretary &ndash; Agriculture &amp; cooperation and Home affairs. He also held the position of CEO of the Greater Noida Industrial Development Authority.</p>\\r\\n\\r\\n<p>He has rich experience in the sectors of Rural Development, Public Works, Urban Development, Agriculture and Cooperation, Home Affairs, Commerce, Health and Financial Services etc.</p>\\r\\n\\r\\n<p>Additional information:</p>\\r\\n\\r\\n<p>He is known for his approach for bringing in reforms and transforming / significant changes &ndash; with the help of technology.</p>\\r\\n\\r\\n<p>Mr. Panda&#39;s impressive career includes pioneering the Dial 112 &ndash; Police Emergency Response System, a game-changing initiative that saved lives during emergencies. His contributions extended to enhancing law enforcement efficiency as the Principal Secretary, Department of Home, Government of Uttar Pradesh.</p>\\r\\n\\r\\n<p>Since taking up his role at IRDAI, Mr. Panda has initiated a comprehensive reform agenda with the overarching goal of achieving &quot;Insurance for all&quot; by 2047. His visionary leadership has brought about ease of doing business, operational ease and flexibility, reduction in compliance burden and costs in the insurance sector thereby fostering a more inclusive and accessible insurance landscape. He has adopted a customer centric approach keeping the interests of policyholders at the forefront. He is credited, and rightfully so, for bringing in progressive, supportive, facilitative and a forward looking regulatory architecture in insurance sector.</p>\\r\\n\\r\\n<p>Mr. Panda&#39;s relentless efforts and forward-thinking approach have undeniably eased the lives of the common man and exemplify his dedication towards welfare of public interests, leading to the growth and development of the nation as a whole.</p>\\r\\n', '', 'Shri Debasish Panda.png', 'Shri', 'VIP', 1, 2, 'debasish-panda'),
-(4, 'CHARLES COWLING', 'President', '<p>Charles is the President of the International Actuarial Association. He has helped to lead on many of their initiatives and key projects, including partnering with a number of other supranational organisations. He is currently helping to lead the IAA&rsquo;s work around AI and the role of the actuary of the future. Charles is also the Chief Actuary of Mercer in the UK. He has acted as principal actuarial adviser to several multinational companies and their pension schemes and has advised on global de-risking projects.</p>\\r\\n\\r\\n<p>Charles has pioneered the use of modern corporate finance techniques in actuarial work and was Chairman of the Institute and Faculty of Actuaries&rsquo; (IFoA&rsquo;s) Discount Rate Steering Committee which led the UK Profession&#39;s research into the use of discount rates in actuarial work. He is a member of the Council of the IFoA and has chaired their Pensions Board and their International Board. Charles is the author of a number of research papers and a regular speaker at conferences around the world.</p>\\r\\n\\r\\n<p>Charles is a Fellow of the Institute of Actuaries. He has a BSc from Durham University and he is also a Chartered Mathematician, a Chartered Scientist and a Fellow of the Institute of Mathematics and its Applications. &nbsp;He is also a recently ordained minister in the Church of England.</p>\\r\\n', '', 'charles.png', 'Mr', 'NON VIP', 1, 4, 'charles-cowling');
-
---
--- Triggers `speakers`
---
-DELIMITER $$
-CREATE TRIGGER `update_order_number` BEFORE INSERT ON `speakers` FOR EACH ROW BEGIN
-    -- Set the order_number column with the value from the auto-incremented id column
-    SET NEW.order_number = (SELECT AUTO_INCREMENT FROM information_schema.tables WHERE table_name = 'speakers' AND table_schema = DATABASE());
-END
-$$
-DELIMITER ;
+INSERT INTO `posts` (`id`, `name`, `email`, `contact`, `member_id`, `caption`, `image_path`, `status`, `created_at`) VALUES
+(2, 'Faheem', 'sk3136537@gmail.com', '8070139237', '', 'ok', 'uploads/memes/image_6593b606022c6.png', '1', '2024-01-02 07:06:46'),
+(3, 'Faheem', 'sk3136537@gmail.com', '8070139237', '', 'ok', 'uploads/memes/image_6593b71a72b20.png', '1', '2024-01-02 07:11:22'),
+(4, 'Faheem', 'sk3136537@gmail.com', '8070139237', '', 'ok', 'uploads/memes/image_6593b7e35a551.jpg', '0', '2024-01-02 07:14:43'),
+(5, 'Faheem', 'sk3136537@gmail.com', '8070139237', '', 'ok', 'uploads/memes/image_6593b82d4880c.jpg', '0', '2024-01-02 07:15:57');
 
 -- --------------------------------------------------------
 
@@ -205,9 +194,12 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `first_name`, `surname`, `member_id`, `actuarial_assoc`, `organization`, `email`, `contact_no`, `status`, `created_at`) VALUES
-(1, 'faheem', 'kjdhf', 0, 'Institute & Faculty of Actuaries', 'iai', 'djkfa@gmail.com', '9076543212', 1, '2024-01-12 16:14:37'),
-(2, 'faheem', 'djhfjk', 15, 'Institute of Actuaries of India', 'jdhfkj', 'jfhjdhfjk@gmail.com', '9887777727', 1, '2024-01-12 16:26:22'),
-(3, 'sdjfjhskfh', 'kjhjkh', 16, 'Institute of Actuaries of India', 'hghjg', 'hjgjhghj@gmail.com', '9876543213', 1, '2024-01-12 16:27:29');
+(1, 'Faheem', 'Shaikh', 23, 'Institute & Faculty of Actuaries', 'IAI', 'sk@gmail.com', '8909877662', 1, '2024-01-10 18:31:52'),
+(2, 'sumit', 'patil', 87987, 'Institute & Faculty of Actuaries', 'kjhdfkj', 'jkhj@gmail.com', '87987', 1, '2024-01-11 16:11:37'),
+(3, 'jamwal', 'djfkh', 123, 'Institute & Faculty of Actuaries', 'jgdsf', 'hjg@gmail.com', 'dfdfd', 1, '2024-01-11 16:13:33'),
+(4, 'larry', 'barretto', 50001, 'Casualty Actuarial Society', 'dfdf', 'larry@actuariesindia.org', '98098765443', 1, '2024-01-11 16:33:56'),
+(5, 'rashi', 'patel', 50002, 'Institute & Faculty of Actuaries', 'IAI', 'rashi@actuariesindia.org', '9087890987', 1, '2024-01-11 16:51:41'),
+(6, 'Sumit', 'patil', 5003, 'Casualty Actuarial Society', 'ghjdfg', 'sumit@actuariesindia.org', '8909876543', 1, '2024-01-11 17:58:00');
 
 -- --------------------------------------------------------
 
@@ -224,8 +216,51 @@ CREATE TABLE `user_submissions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `user_submissions`
+--
+
+INSERT INTO `user_submissions` (`id`, `user_id`, `question_id`, `user_answer`, `submission_time`) VALUES
+(1, 1, 1, '3', '2024-01-11 09:32:52'),
+(2, 1, 2, '2', '2024-01-11 09:32:52'),
+(3, 1, 3, '1', '2024-01-11 09:32:52'),
+(4, 1, 4, '1', '2024-01-11 09:32:52'),
+(5, 1, 5, '3', '2024-01-11 09:32:52'),
+(6, 3, 1, '0', '2024-01-11 10:43:47'),
+(7, 3, 2, '2', '2024-01-11 10:43:48'),
+(8, 3, 3, '3', '2024-01-11 10:43:48'),
+(9, 3, 4, '1', '2024-01-11 10:43:48'),
+(10, 3, 5, '2', '2024-01-11 10:43:48'),
+(11, 4, 1, '1', '2024-01-11 11:12:00'),
+(12, 4, 2, '2', '2024-01-11 11:12:00'),
+(13, 4, 3, '3', '2024-01-11 11:12:00'),
+(14, 4, 4, '2', '2024-01-11 11:12:00'),
+(15, 4, 5, '1', '2024-01-11 11:12:00'),
+(16, 5, 1, '0', '2024-01-11 11:22:36'),
+(17, 5, 2, '0', '2024-01-11 11:22:36'),
+(18, 5, 3, '1', '2024-01-11 11:22:36'),
+(19, 5, 4, '2', '2024-01-11 11:22:36'),
+(20, 5, 5, '2', '2024-01-11 11:22:36'),
+(21, 6, 1, '3', '2024-01-11 12:28:56'),
+(22, 6, 2, '1', '2024-01-11 12:28:56'),
+(23, 6, 3, '0', '2024-01-11 12:28:56'),
+(24, 6, 4, '1', '2024-01-11 12:28:56'),
+(25, 6, 5, '2', '2024-01-11 12:28:56');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `dislikes`
+--
+ALTER TABLE `dislikes`
+  ADD PRIMARY KEY (`dislike_id`);
+
+--
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`like_id`);
 
 --
 -- Indexes for table `mcq_questions`
@@ -234,21 +269,9 @@ ALTER TABLE `mcq_questions`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `meme_users`
+-- Indexes for table `posts`
 --
-ALTER TABLE `meme_users`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `meme_votes`
---
-ALTER TABLE `meme_votes`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `speakers`
---
-ALTER TABLE `speakers`
+ALTER TABLE `posts`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -274,28 +297,28 @@ ALTER TABLE `user_submissions`
 --
 
 --
+-- AUTO_INCREMENT for table `dislikes`
+--
+ALTER TABLE `dislikes`
+  MODIFY `dislike_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `like_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
 -- AUTO_INCREMENT for table `mcq_questions`
 --
 ALTER TABLE `mcq_questions`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `meme_users`
+-- AUTO_INCREMENT for table `posts`
 --
-ALTER TABLE `meme_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `meme_votes`
---
-ALTER TABLE `meme_votes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `speakers`
---
-ALTER TABLE `speakers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tbl_pregcatour`
@@ -307,13 +330,13 @@ ALTER TABLE `tbl_pregcatour`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `user_submissions`
 --
 ALTER TABLE `user_submissions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
